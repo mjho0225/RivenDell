ae95b9490baec25953291e40c7574642d98301e2
mjho0225 <52567731+mjho0225@users.noreply.github.com>
2019-08-14 18:04:51 +0900

    파란색 화살표 완료

---
+++ RivenDell/Assets/02.Scripts/ManagerWatch.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+
+public class ManagerWatch : MonoBehaviour
+{
+    public GameObject WatchUI; //시계 UI
+    public Transform handPivot; //시침분침 피봇
+    public Transform bigWatch;
+    public Transform emptyPivot;
+
+    public GameObject target; // 태양 타겟
+
+    int angle; //각도
+    int sunMask;
+
+    float i; // 화살표 숫자
+
+    public Slider[] m_ArrowSlider; // 화살표 배열
+
+    float rayLength = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WatchUI = GetComponentInChildren<Canvas>().gameObject;
+        WatchUI.SetActive(false);
+
+        sunMask = LayerMask.GetMask("SUN");
+
+        StartCoroutine("OnTriggerEnter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+            angle = UnityEngine.Random.Range(1, 12);
+
+            handPivot.localEulerAngles = new Vector3(0, 0, angle * 30);
+        while (coll.CompareTag("RIGHTHAND"))
+        {           
+            Debug.Log("들어와썽!");
+            WatchUI.SetActive(true);
+            m_ArrowSlider[0].gameObject.SetActive(true);
+            
+            //12 개의 시간 방향 중 랜덤하게 1개
+            coll.enabled = false;
+            
+        }
+        StartCoroutine("WaitRaycast");
+    }
+}
--- RivenDell/Assets/02.Scripts/WatchMgr.cs
+++ RivenDell/Assets/02.Scripts/WatchMgr.cs
@@ -10,6 +10,7 @@ public class WatchMgr : MonoBehaviour
     public GameObject WatchUI; //시계 UI
     public Transform handPivot; //시침분침 피봇
     public Transform bigWatch;
+    public Transform emptyPivot;
    
     public GameObject target; // 태양 타겟
 
@@ -17,13 +18,16 @@ public class WatchMgr : MonoBehaviour
     int sunMask;
 
     float i; // 화살표 숫자
+    Vector3 mid;
 
     public Slider[] m_ArrowSlider; // 화살표 배열
 
     float rayLength = 1000f;
     float currTime; //현재시간
+    
 
     bool wait = false;
+    bool ok = false;
 
     Ray ray;
     RaycastHit hit; //레이힛
@@ -43,16 +47,11 @@ public class WatchMgr : MonoBehaviour
         WatchUI = GetComponentInChildren<Canvas>().gameObject;
         WatchUI.SetActive(false);
 
-
-
         sunMask = LayerMask.GetMask("SUN");
         //시침의 방향 랜덤생성(12개)
         //HandPivot의 Rotation의 Y값을 1~12까지의 12개 값에 30을 곱해 360도 단위로 만든다.
-
-        
-
-        rb = GetComponent<Rigidbody>();
-
+       
+        rb = GetComponent<Rigidbody>();       
     }
 
     // Update is called once per frame
@@ -77,8 +76,18 @@ public class WatchMgr : MonoBehaviour
 
 
         Vector3 dir = target.transform.position - handPivot.transform.position;
+        mid = handPivot.up + bigWatch.forward;
 
         //float dot = Vector3.Dot(handPivot.localEulerAngles, dir);
+        //Vector3 v = dir - handPivot.localEulerAngles;
+        //float m = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+
+
+        Debug.Log("태양의 v값" + dir);
+        Debug.Log("12의 값" + bigWatch.forward);
+        
+
+        //Debug.Log("m : 각도값" + m);
 
         //if (Mathf.Approximately(a,b))
         // {
@@ -90,21 +99,39 @@ public class WatchMgr : MonoBehaviour
         //{
 
         //}
-        
+
+
         ray = new Ray(handPivot.position, handPivot.up);
 
+        
+        Debug.DrawRay(emptyPivot.position, mid, Color.blue);
         Debug.DrawRay(handPivot.position, handPivot.up, Color.red);
+        Debug.DrawRay(bigWatch.position, dir, Color.yellow);
         Debug.DrawRay(bigWatch.position, bigWatch.forward, Color.green);
+
+        
+
 
         //만약 시계 UI가 생성되면 레이캐스를 쏴라
 
-        if(wait == true)
+        if (wait == true)
         {
-        WaitRaycast();
+            WaitRaycast();
+            
         }
+        /*if(ok == true)
+        {
+            SumMid();
+        }*/
     }
+    /*public float GetAngle(handPivot.localEulerAngles, Vector3 vEnd)
+    {
+        Vector3 v = vEnd - handPivot.localEulerAngles;
 
-    
+        return Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+    }*/
+
+
 
     //시계 트리거 엔터 되면 UI 오픈
     private void OnTriggerEnter(Collider coll)
@@ -124,6 +151,7 @@ public class WatchMgr : MonoBehaviour
             handPivot.localEulerAngles = new Vector3(0, 0, angle * 30);
 
             coll.enabled = false;
+            
         }
     }
     void Slider_G()
@@ -139,8 +167,9 @@ public class WatchMgr : MonoBehaviour
     private void Slider_R()
     {       
             m_ArrowSlider[1].value = currTime;
+        m_ArrowSlider[2].value = currTime;
 
-            if (currTime > 1)
+        if (currTime > 1)
             {
                 currTime = 1;
 
@@ -148,42 +177,76 @@ public class WatchMgr : MonoBehaviour
     }
     private void Slider_B()
     {
-        m_ArrowSlider[2].value = currTime;
+       /* m_ArrowSlider[2].value = currTime;
 
         if (currTime > 1)
         {
             currTime = 1;
 
-        }
+        }*/
     }
 
     void WaitRaycast()
     {
         if (Physics.Raycast(ray, out hit, rayLength, sunMask))
         {
-            currTime = 0;
+            
             Debug.Log("검출하였습니다.");
-            //빨간색화살표 생성
-            m_ArrowSlider[1].gameObject.SetActive(true);
-            SN = SliderNumber.Slider_R;
-            target.GetComponent<Collider>().enabled = false;
             //시계UI 고정
-            if(transform != null)
+            if (transform != null)
             {
                 transform.parent = transform.parent.parent; // 부모로부터 상속해제 후 부모와 동일
 
                 //transform.rotation = Quaternion.Euler(0, target., 0);
                 //transform.rotation = Quaternion.identity; // 로테이션값 초기화
-                transform.eulerAngles = new Vector3(0,transform.eulerAngles.y, 0); //태양 방향의 값으로 평면화
+                transform.eulerAngles = new Vector3(-93, transform.eulerAngles.y, transform.eulerAngles.z); //태양 방향의 값으로 평면화
 
                 /*rb.constraints = RigidbodyConstraints.FreezeRotationX;
                 rb.constraints = RigidbodyConstraints.FreezeRotationY;
                 rb.constraints = RigidbodyConstraints.FreezeRotationZ;
                 rb.constraints = RigidbodyConstraints.FreezePositionY;*/
             }
-            
+            target.GetComponent<Collider>().enabled = false;
 
+            SN = SliderNumber.Slider_R;
+            //빨간색화살표 생성
+            m_ArrowSlider[1].gameObject.SetActive(true);
+            currTime = 0;
+            
+            m_ArrowSlider[2].gameObject.SetActive(true);
+            //SN = SliderNumber.Slider_B;
+
+            //emptyPivot.localRotation = Quaternion.LookRotation(mid);
+
+            //ok = true;
+
+            float Dot = Vector3.Dot(handPivot.up, bigWatch.forward);
+            float angle2 = Mathf.Acos(Dot) * Mathf.Rad2Deg;
+            emptyPivot.localEulerAngles = new Vector3(0, 0, -angle2/2);
+            Debug.Log("angle2 : " + angle2);
 
         }
+        
     }
+
+    /*void SumMid()
+    {
+        //만약 빨간색 일치가 된다면 파란화살표를 생성하라
+        //파란색을 중간값에 나타내라
+        if (ok == true)
+        {
+            m_ArrowSlider[2].gameObject.SetActive(true);
+            SN = SliderNumber.Slider_B;
+            
+
+            Vector3 mid = handPivot.up + bigWatch.forward;
+
+            emptyPivot.localRotation = Quaternion.LookRotation(mid);
+            //emptyPivot.localEulerAngles = new Vector3(0, 0, );
+
+            Debug.Log("mid 의 값" + mid);
+            Debug.DrawRay(bigWatch.position, mid, Color.blue);
+        }
+    }*/
+
 }
