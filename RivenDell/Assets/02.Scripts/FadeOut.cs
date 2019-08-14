ed1890b7c208671ff2403d00b02e8bfca9496cfb
BornsWagger <54009825+BornsWagger@users.noreply.github.com>
2019-08-14 16:27:30 +0900

    Fade In/Out
    Fade In/Out 및 검은 대기 씬 구현

---
+++ RivenDell/Assets/02.Scripts/FadeOut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    public Image fadeImg;
+    float fades = 0.0f;
+    float time = 0;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        
+        if(fades < 1.0f && time >= 0.1f)
+        {
+            fades += 0.1f;
+            fadeImg.color = new Color(0, 0, 0, fades);
+            time = 0;
+        }
+        else if (fades >= 1.0f)
+        {
+
+            time = 0;
+        }
+    }
+}
