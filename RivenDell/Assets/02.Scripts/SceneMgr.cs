9d6dd76bcbf567e0e38536ac465313af09d9f6c5
mjho0225 <52567731+mjho0225@users.noreply.github.com>
2019-08-20 13:32:04 +0900

    인트로캔버스 수정

--- RivenDell/Assets/02.Scripts/SceneMgr.cs
+++ RivenDell/Assets/02.Scripts/SceneMgr.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 public class SceneMgr : MonoBehaviour
 {
     Transform spawnPoint;
+    Transform startPoint;
+    GameObject watchUI;
     // Start is called before the first frame update
     void Start()
     {
+        startPoint = GameObject.Find("StartPoint").GetComponent<Transform>();
         spawnPoint = GameObject.Find("SpawnPoint").GetComponent<Transform>();
+        //watchUI = GameObject.Find("Direction_Canvas").GetComponent<GameObject>();
     }
 
     // Update is called once per frame
@@ -19,8 +23,13 @@ public class SceneMgr : MonoBehaviour
     }
     private void OnLevelWasLoaded(int level)
     {
+        if(level == 1)
+        {
+            transform.position = startPoint.position;
+        }
         if(level == 2)
         {
+            //Destroy(watchUI);
             transform.position = spawnPoint.position;//new Vector3(80, 32.7f, 158);
         }
     }
