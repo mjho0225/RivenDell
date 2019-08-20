faa3943e72879fdec3543e15cd16f9a0a4581882
BornsWagger <54009825+BornsWagger@users.noreply.github.com>
2019-08-20 11:08:23 +0900

    인트로 화면 생성
    ㅇㅇ

---
+++ RivenDell/Assets/02.Scripts/Twinkle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Twinkle : MonoBehaviour
+{
+
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //나는 게임오브젝트의 알파를 0에서 255까지 왔다갔다 하게 하고싶다.
+        text = GetComponent<Text>();
+        float changeAlpha = text.color.a;
+        changeAlpha--;
+        
+        
+    }
+}
