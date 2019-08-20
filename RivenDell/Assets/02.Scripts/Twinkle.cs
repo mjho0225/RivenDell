b675e61932497da5aa9b7dfc0dd0e599c67e8314
BornsWagger <54009825+BornsWagger@users.noreply.github.com>
2019-08-20 11:32:13 +0900

    인트로 씬 저장
    ㅇㅇ

--- RivenDell/Assets/02.Scripts/Twinkle.cs
+++ RivenDell/Assets/02.Scripts/Twinkle.cs
@@ -6,21 +6,27 @@ using UnityEngine.UI;
 public class Twinkle : MonoBehaviour
 {
 
-    public Text text;
+    public GameObject text;
 
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //나는 게임오브젝트의 알파를 0에서 255까지 왔다갔다 하게 하고싶다.
-        text = GetComponent<Text>();
-        float changeAlpha = text.color.a;
-        changeAlpha--;
+        //나는 텍스트의 알파를 0에서 255까지 왔다갔다 하게 하고싶다.
+        //나는 텍스트가 깜빡이게 하고 싶다.
+        for(int i=0; i<3; i++)
+        {
+            gameObject.SetActive(false);
+            gameObject.SetActive(true);
+        }
+     
+        
         
         
     }
