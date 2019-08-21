2c7dda1bcdf91591f93322a4912981ad26448918
mjho0225 <52567731+mjho0225@users.noreply.github.com>
2019-08-21 11:11:46 +0900

    hitpoint 테스트

---
+++ RivenDell/Assets/02.Scripts/HitPointPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitPointPosition : MonoBehaviour
+{
+    public GameObject[] hitPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
